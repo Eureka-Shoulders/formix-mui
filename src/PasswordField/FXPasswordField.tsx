5035ca98ed944ae20c3759a43106a3793aa9c4de
Yuri <yuri@eurekalabs.com.br>
2022-04-27 09:08:11 -0300

    fix: adding textfield props on FXPasswordField

--- src/PasswordField/FXPasswordField.tsx
+++ src/PasswordField/FXPasswordField.tsx
@@ -1,18 +1,20 @@
 import { observer } from 'mobx-react-lite';
 import { useField } from '@euk-labs/formix';
 import { PasswordField } from '@euk-labs/componentz';
+import type { TextFieldProps } from '@mui/material/TextField';
 
-interface FieldProps {
+type FieldProps = {
   name: string;
   label?: string;
-}
+} & TextFieldProps;
 
-function FXPasswordField({ name, label }: FieldProps) {
+function FXPasswordField({ name, label, ...rest }: FieldProps) {
   const { field, meta } = useField(name);
 
   return (
     <PasswordField
       {...field}
+      {...rest}
       label={label}
       disabled={meta.disabled}
       error={meta.touched && !!meta.error}
